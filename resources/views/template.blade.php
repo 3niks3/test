097a86e2415459a1584e50a2fc1e0cb3f8b2c4ee
Austris <austrisbaumanis@gmail.com>
2015-05-15 19:09:26 +0300

    First Commit

---
+++ resources/views/template.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Banka</title>
+</head>
+<body>
+		@yield('nav')
+	<div class="container">
+		@yield('content')
+	</div>
+</body>
+</html>
