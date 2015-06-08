d6f0ad07fe2133c4a780e251911f05583d506dc5
Cracken6 <krims.lv@gmail.com>
2015-06-08 14:19:10 +0300

    basic theme update #1

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Banka</title>
-</head>
-<body>
-
-
-</body>
-</html>
-
 <html lang="en"><head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
