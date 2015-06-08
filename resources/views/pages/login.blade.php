19bcbe4bdaf9301c5e5a9976d13a9576c141dd70
Cracken6 <krims.lv@gmail.com>
2015-06-08 14:15:03 +0300

    basic theme

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,5 +1,8 @@
 @extends('template')
 	@section('content')
-	<p>test</p>
-    <p>test2</p>
+
+        <legend>title</legend>
+
+        test
+
 	@stop
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,9 +5,66 @@
 	<title>Banka</title>
 </head>
 <body>
-		@yield('nav')
-	<div class="container">
-		@yield('content')
-	</div>
+
+
 </body>
 </html>
+
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>iBanka</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+<body>
+
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">iBanka</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Sākums</a></li>
+                <li><a href="#">Maksājumi</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+
+<div class="container">
+
+
+
+
+    <div class="panel panel-default">
+        <div class="panel-body">
+            @yield('content')
+        </div>
+    </div>
+
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+
+
+</body></html>
