8b4feaf539427444083773baf3f5ebac62c7412c
Austris <austrisbaumanis@gmail.com>
2015-06-17 02:20:39 +0300

    Login_history modelis

---
+++ app/Login_history.php
@@ -0,0 +1,13 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Login_history extends Model {
+
+    protected $primaryKey = 'hist_ID';
+    protected $table = 'login_history';
+
+    protected $fillable = ['hist_user_ID', 'hist_timestamp', 'hist_IP'];
+
+
+}
